69eceac55b18ce1cd03e432336769b0e24a9e7ae
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-02 23:35:26 +0700

    create interface IUser

---
+++ src/main/java/service/userService/IUser.java
@@ -0,0 +1,12 @@
+package service.userService;
+
+import model.User;
+
+import java.util.List;
+
+public interface IUser {
+    List<User> findAll();
+    User findById(int id);
+    boolean create(User user);
+    boolean update(User user);
+}
